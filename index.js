5c1ca0163bfcc04644a1447edcbba1b519bc7705
Brady Shinners <brady.shinners@gmail.com>
2020-03-10 01:01:07 -0500

    Add current full lint stack

---
+++ index.js
@@ -0,0 +1,7 @@
+const stylelintConfig = require('./stylelint.config')
+const prettierConfig = require('./prettier.config')
+const eslintrc = require('./\.eslintrc')
+
+exports.eslintrc = eslintrc
+exports.stylelintConfig = stylelintConfig
+exports.prettierConfig = prettierConfig
